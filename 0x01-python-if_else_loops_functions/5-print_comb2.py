1cea35fc9a3e778d9ebf517dde977357caec5263
Armolas <armolas06@gmail.com>
2023-10-08 00:43:21 +0000

    aargg

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 sep = ", "
 for num in range(100):
-    print("{:02d}".format(num), end=sep)
-print()
+    if num != 99:
+        print("{:02d}".format(num), end=sep)
+    else:
+        print("{:02d}".format(num))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ for i in range(10):
     for j in range(10):
         if j > i:
             print("{:d}".format(i), end="")
-            print("{:d}".format(j), end=", ")
-print()
+            if i != 8:
+                print("{:d}".format(j), end=", ")
+            else:
+                print("{:d}".format(j))
