f0fc599263fc9ddbe47bf1e06b3e257468d29ad7
Armolas <armolas06@gmail.com>
2023-10-08 13:48:29 +0000

    slow and fast

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,12 +16,10 @@ int check_cycle(listint_t *list)
 		return (i);
 	while (fast && slow)
 	{
-		if (slow->next)
-			slow = slow->next;
-		else
-			break;
-		if (fast->next->next)
-			fast = fast->next->next;
+		slow = slow->next;
+		fast = fast->next;
+		if (fast->next)
+			fast = fast->next;
 		else
 			break;
 		if (slow == fast)
