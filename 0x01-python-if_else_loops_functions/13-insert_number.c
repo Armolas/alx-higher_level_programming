65bc33b4d87e4a2c53f4a98f980da0f070be1cbb
Armolas <armolas06@gmail.com>
2023-10-09 22:23:32 +0000

    Tech Interview Prep

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,11 +28,10 @@ listint_t *insert_node(listint_t **head, int number)
 		{
 			new->next = current_next;
 			current->next = new;
-			return (new);
 		}
 		current = current->next;
 	}
 	new->next = NULL;
-	current->next = new;
+	current = new;
 	return (new);
 }
