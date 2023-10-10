73cb8760e12cf2386397568431a1b0cb80697c3f
Armolas <armolas06@gmail.com>
2023-10-10 01:53:41 +0000

    import modules

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    copy = my_list.copy()
     for i in range(len(my_list)):
-        print("{:d}".format(my_list.pop()))
+        print("{:d}".format(copy.pop()))
