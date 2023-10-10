a3fd929e280d70935de5f85250416632d5e81e5a
Armolas <armolas06@gmail.com>
2023-10-10 00:09:00 +0000

    import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    no_of_args = len(argv) - 1
+    if no_of_args == 0:
+        print("0 arguments.")
+    elif no_of_args == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(no_of_args))
+        for i in range(1, no_of_args + 1):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
+    
