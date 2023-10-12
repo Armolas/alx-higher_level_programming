1a385d434376cd5babecae50f984ecbd2492c064
Armolas <armolas06@gmail.com>
2023-10-12 00:11:42 +0000

    lambda, sets

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = reduce(lambda x, y: x + y, list(set(my_list)))
+    uniq = list(set(my_list))
+    add = 0
+    for i in range(len(uniq)):
+        add += uniq[i]
     return add
