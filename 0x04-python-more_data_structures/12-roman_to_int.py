ff4039cd69bf8ef5943b5dbe69d561162553e1db
Armolas <armolas06@gmail.com>
2023-10-14 00:13:08 +0000

    roman to num

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+    if roman_string != str(roman_string):
+        return 0
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     num = 0
     i = 0
