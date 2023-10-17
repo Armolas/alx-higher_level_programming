f975f8a27e771aee0b551d7f3b81db6129f26194
Armolas <armolas06@gmail.com>
2023-10-17 23:45:46 +0000

    adv

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(strn, n):
+    string = ""
+    for i in range(len(strn)):
+        if i == n:
+            continue;
+        string += (strn[i])
+    return string
