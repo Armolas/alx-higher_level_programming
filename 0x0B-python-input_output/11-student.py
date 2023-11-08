458272475b98340702673fc95b7375aa3f1be68b
Armolas <armolas06@gmail.com>
2023-11-08 23:17:28 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains the class Student"""
+
+
+class Student:
+    """creates an instance of a student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes a student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if (isinstance(attrs, list) and all
+                (isinstance(elm, str) for elm in attrs)):
+            my_dict = {}
+            for elm in attrs:
+                if elm in self.__dict__:
+                    my_dict[elm] = self.__dict__[elm]
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            if key in self.__dict__:
+                setattr(self, key, json[key])
