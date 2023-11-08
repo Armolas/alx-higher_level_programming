e31edd9c2432a43d0d66cd6d9bb5a628bf3544a7
Armolas <armolas06@gmail.com>
2023-11-08 21:11:58 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,20 +5,12 @@
 if __name__ == "__main__":
     import sys
     """This is the sys module"""
-
-
     import os
     """This is the os module"""
-
-
     load = __import__('6-load_from_json_file').load_from_json_file
     """This module loads an object from a json file"""
-
-
     save = __import__('5-save_to_json_file').save_to_json_file
     """This module saves an object to a json file"""
-
-
     no_of_args = len(sys.argv)
     if os.path.exists("add_item.json"):
         my_list = load('add_item.json')
