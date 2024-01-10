e50beb1ea28c8f9289d4f222baca48fdf5be8d68
Armolas <armolas06@gmail.com>
2024-01-10 03:53:41 +0100

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const Rectangle = class {
+  constructor (w, h) {
+    if (w < 1 || h < 1 || isNaN(w) || isNaN(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let j = 0; j < this.width; j++) {
+        rect = rect + 'X';
+      }
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+module.exports = Rectangle;
