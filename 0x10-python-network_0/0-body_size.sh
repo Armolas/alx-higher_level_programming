49603e1b641c5d9ed6b938159f038a5e8774165d
Armolas <armolas06@gmail.com>
2024-01-26 00:46:51 +0100

    shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #sends a request to that URL, and displays the size of the body of the response
 curl -sI $1 | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n'
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #displays all HTTP methods the server will accept.
 curl -sIX OPTIONS $1 | grep -i 'Allow' | cut -d ' ' -f 2-
