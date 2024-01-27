26d7631253de00abcc2c5bd9868318274f500f56
Armolas <armolas06@gmail.com>
2024-01-27 07:22:26 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Handles Error code from request"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code: " + e.code)
