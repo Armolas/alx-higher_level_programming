ab0d1919051225c0406613fea2c3d7de6080e4ad
Armolas <armolas06@gmail.com>
2024-01-27 07:07:23 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")\
+            as status:
+        html = status.read()
+        print("Body response:")
+        print("    - type:", type(html))
+        print("    - content:", html)
+        print("    - utf8 content:", html.decode('utf-8'))
