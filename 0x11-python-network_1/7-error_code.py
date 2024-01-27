3441b4d233e6e109484522e610caa4c5b7c11379
Armolas <armolas06@gmail.com>
2024-01-27 09:47:13 +0100

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""handles error from requests"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
