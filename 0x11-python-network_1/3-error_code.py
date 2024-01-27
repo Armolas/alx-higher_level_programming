df31b7638f7160e06a96f4bbad279e9746da3662
Armolas <armolas06@gmail.com>
2024-01-27 07:29:40 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-        print("Error code:",e.code)
+        print(f"Error code: {e.code}")
