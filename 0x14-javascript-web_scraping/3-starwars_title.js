af7a34e8e76ad21037b3e0f641f97d917b0e3979
Armolas <armolas06@gmail.com>
2024-02-21 01:32:39 +0100

    tasks_3_and_2

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const film = JSON.parse(body);
+  console.log(film.title);
+});
